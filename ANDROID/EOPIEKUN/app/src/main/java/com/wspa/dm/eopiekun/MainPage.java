097a496f574553f062710f40d3abd6a1d6934432
Tomasz Malarczyk <malarczykt@gmail.com>
2017-06-12 20:17:36 +0200

    eopiekun 1.02
    obsluga niezalogowanego w glownej aktywnosci

--- ANDROID/EOPIEKUN/app/src/main/java/com/wspa/dm/eopiekun/MainPage.java
+++ ANDROID/EOPIEKUN/app/src/main/java/com/wspa/dm/eopiekun/MainPage.java
@@ -1,19 +1,54 @@
 package com.wspa.dm.eopiekun;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainPage extends AppCompatActivity {
  Button button1, button2;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         button1=(Button) findViewById(R.id.profbtn);
         button2=(Button) findViewById(R.id.testloc);
+        auth = FirebaseAuth.getInstance();
+
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainPage.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+
+
+
+
+
+
+
+
+
+
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,4 +74,20 @@ public class MainPage extends AppCompatActivity {
 
 
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
+
 }
